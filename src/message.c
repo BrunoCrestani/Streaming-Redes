51bbdfd1069992a8c98dbec2a2f378fc64ae3151
BrunoCrestani <bruno.crestani@ufpr.br>
2024-06-26 17:42:51 +0000

    Dando inicio a estruturacao da mensagem

--- src/message.c
+++ src/message.c
@@ -1,14 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "message.h"
+
+#define MAX_DATA_SIZE 63
 
 #define ACK 00000
 #define NACK 00001
-#define LISTA 01010
-#define BAIXAR 01011
+#define LIST 01010
+#define DOWNLOAD 01011
 #define SHOW 10000
 #define FILE_INFO 10001
 #define DATA 10010
 #define END 11110
 #define ERROR 11111
+
+/*
+ * Creates a message
+ */
+message* createMessage(uint8_t marker, uint8_t size, uint8_t sequence, uint8_t type, uint8_t data[], uint8_t error){
+  message* msg;
+
+  if (!(msg = malloc(sizeof(message))))
+    return NULL;
+
+  msg->marker = marker;
+  msg->size = size;
+  msg->sequence = sequence;
+  msg->type = type;
+  msg->data = mallocData();
+  msg->error = error;
+
+  return msg;
+}
+
+uint8_t* mallocData(){
+  uint8_t* data;
+  
+  if ((data = malloc(sizeof(uint8_t) * MAX_DATA_SIZE))) return data;
+
+  return NULL;
+}
+
+/*
+ * Deletes a fully acknowledged message
+ */
+void deleteMessage(message* msg, unsigned int sizeAck){
+  if (sizeAck == msg->size) free(msg);
+}
+
+/*
+ * The process of sending a 
+ * message from the server to the user
+ */
+unsigned int sendMessage(){
+
+  return NULL;
+}
 
 
--- src/message.h
+++ src/message.h
@@ -1,10 +1,34 @@
+#include <stdint.h>
 
 typedef struct message {
-  unsigned int marker;
-  unsigned int size;
-  unsigned int sequence;
-  unsigned int type;
-  unsigned int* data;
-  unsigned int error;
+  uint8_t marker;
+  uint8_t size: 6;
+  uint8_t sequence: 5;
+  uint8_t type: 5;
+  uint8_t *data;
+  uint8_t error;
   
 } message;
+
+/*
+ * Creates a message
+ */
+message* createMessage(uint8_t marker, uint8_t size, uint8_t sequence, uint8_t type, uint8_t* data, uint8_t error);
+
+/*
+ * memory allocs the necessary space for the data array
+ */
+uint8_t* mallocData();
+
+/*
+ * Deletes a fully acknowledged message
+ */
+void deleteMessage(message* msg, unsigned int sizeAck);
+
+/*
+ * The process of sending a 
+ * message from the server to the user
+ */
+unsigned int sendMessage();
+
+
