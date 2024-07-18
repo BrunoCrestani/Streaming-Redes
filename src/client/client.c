20ff8c92dd988c34061e01466581cc4aaec2bca2
marcelo_augusto <marcelo.rissette@vidyatec.com>
2024-07-17 21:38:49 -0300

    chore: Refactor raw socket communication code and add file append functionality

--- src/client/client.c
+++ src/client/client.c
@@ -3,23 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <net/ethernet.h> 
-#include <linux/if_packet.h> 
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stddef.h> 
-#include <net/if.h> 
+#include <stddef.h>
+#include <net/if.h>
 #include <unistd.h>
-#include <arpa/inet.h> 
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
 #include "../raw_sockets/sockets.h"
 #include "../message/message.h"
 
-void appendFile(char* filename, uint8_t* data, uint8_t size) {
-    FILE* file = fopen(filename, "a+");
+void appendFile(char *filename, uint8_t *data, uint8_t size)
+{
+    FILE *file = fopen(filename, "a+");
 
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Erro ao abrir arquivo");
         return;
     }
@@ -29,28 +31,32 @@ void appendFile(char* filename, uint8_t* data, uint8_t size) {
     fclose(file);
 }
 
-int main() {
+int main()
+{
     int rsocket = rawSocketCreator("enp3s0f3u3");
-    Message* msg = createFakeMessage();
+    Message *msg = createFakeMessage();
 
-    while (1) {
-        int sentBytes = sendMessage(rsocket, msg);
+    int sentBytes = sendMessage(rsocket, msg);
 
-        if (!sentBytes) {
-            perror("Erro ao enviar mensagem");
-        }
+    if (!sentBytes)
+    {
+        perror("Erro ao enviar mensagem");
+    }
 
-        Message* receivedBytes = receiveMessage(rsocket);
+    while (1)
+    {
+        Message *receivedBytes = receiveMessage(rsocket);
 
-        if (receivedBytes == NULL) {
+        if (receivedBytes == NULL)
+        {
             perror("Erro ao receber mensagem");
         }
 
         switch (receivedBytes->type)
         {
-            case DATA:
-                appendFile("README.md", receivedBytes->data, receivedBytes->size);
-                break;
+        case DATA:
+            appendFile("README.md", receivedBytes->data, receivedBytes->size);
+            break;
         }
     }
 
