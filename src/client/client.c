37d27eaf3acff48b1dd8efdd3710699b20c03c71
marcelo-schreiber <marcelorissette15@gmail.com>
2024-08-01 18:45:29 -0300

    Add options to client and add videos

--- src/client/client.c
+++ src/client/client.c
@@ -32,17 +32,10 @@ void appendFile(char *filename, uint8_t *data, uint8_t size)
     fclose(file);
 }
 
-int main()
+void download_file(int rsocket, char *filename)
 {
-    int rsocket = rawSocketCreator("enp3s0f3u3");
 
-    if (rsocket == -1)
-    {
-        fprintf(stderr, "Erro ao criar socket\n");
-        return -1;
-    }
-
-    Message *msg = createMessage(11, 0, DOWNLOAD, "mewing.mp3");
+    Message *msg = createMessage(strlen(filename), 0, DOWNLOAD, filename);
     long int expectedSequence = 0;
     int sentBytes = sendMessage(rsocket, msg);
 
@@ -65,7 +58,7 @@ int main()
             start = timestamp();
             if (expectedSequence == 0)
             {
-                Message *msg = createMessage(11, 0, DOWNLOAD, "mewing.mp3");
+                Message *msg = createMessage(strlen(filename), 0, DOWNLOAD, filename);
                 sendMessage(rsocket, msg);
             }
         }
@@ -80,31 +73,81 @@ int main()
         case DATA:
             if (receivedBytes->sequence == (expectedSequence % MAX_SEQUENCE))
             {
-                printf("Received message with sequence %d\n", receivedBytes->sequence);
                 appendFile("mewing.mp3", receivedBytes->data, receivedBytes->size);
-                Message* ack = createMessage(13, receivedBytes->sequence, ACK, "Acknowledged");
+                Message *ack = createMessage(13, receivedBytes->sequence, ACK, "Acknowledged");
                 expectedSequence++;
                 sendMessage(rsocket, ack);
-            } 
-            else if (calculateCRC8(receivedBytes->data, receivedBytes->size) != receivedBytes->error) {
-                printf("Received message with error\n");
-                Message* ack = createMessage(21, receivedBytes->sequence, ACK, "Acknowledged");
+            }
+            else if (calculateCRC8(receivedBytes->data, receivedBytes->size) != receivedBytes->error)
+            {
+                Message *ack = createMessage(21, receivedBytes->sequence, ACK, "Acknowledged");
                 sendMessage(rsocket, ack);
             }
-            else {
-                printf("Received out of order message\n");
-                Message* ack = createMessage(21, (expectedSequence - 1) % MAX_SEQUENCE, ACK, "Out of order message");
+            else
+            {
+                Message *ack = createMessage(21, (expectedSequence - 1) % MAX_SEQUENCE, ACK, "Out of order message");
                 sendMessage(rsocket, ack);
             }
-            
+
             break;
 
         case END:
-            Message* ack = createMessage(13, receivedBytes->sequence, ACK, "Acknowledged");
+            Message *ack = createMessage(13, receivedBytes->sequence, ACK, "Acknowledged");
             sendMessage(rsocket, ack);
             printf("Received END message\n");
-            return 0;
+            return;
+        case ERROR:
+            printf("Error message received\n");
+            return;
+        }
+    }
+
+    free(msg);
+
+    return;
+}
+
+int main()
+{
+    int rsocket = rawSocketCreator("enp3s0f3u3");
+
+    if (rsocket == -1)
+    {
+        fprintf(stderr, "Erro ao criar socket\n");
+        return -1;
+    }
+
+    while (1)
+    {
+        char option;
+        printf("baixar aquivo (d)\n");
+        printf("listar arquivos (l)\n");
+        printf("sair (q)\n");
+        printf("\nOpção: ");
+        scanf("%c", &option);
+        system("clear");
+
+        switch (option)
+        {
+        case 'd':
+            char *filename = malloc(FILENAME_MAX);
+
+            for (int i = 0; i < FILENAME_MAX; i++)
+            {
+                filename[i] = '\0';
+            }
+            
+            printf("Digite o nome do arquivo: ");
+            scanf("%s", filename);
+            download_file(rsocket, filename);
+            free(filename);
+
             break;
+        case 'l':
+            list_files(rsocket);
+            break;
+        case 'q':
+            return 0;
         }
     }
 
